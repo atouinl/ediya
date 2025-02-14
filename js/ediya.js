e8e0513995454ef0a591a0c1da8deec033a220c9
atouinl <atouinl@naver.com>
2025-02-14 18:21:13 +0900

    20250214_769px

--- js/ediya.js
+++ js/ediya.js
@@ -16,24 +16,30 @@ m_xicon.onclick = function () {
 
 //왼쪽 이미지 드롭다운
 
-//드롭다운 메뉴바 버튼
-var dropdown = document.querySelector('.mobile_arrow');
-//드롭다운 메뉴?컨텐츠내용
-var dropdownContent = document.querySelector('.mobile_submenu');
-// 아이콘 2개 업과 다운
-var drop_icon1 = document.querySelector('.down');
-var drop_icon2 = document.querySelector('.up');
+//드롭다운 메뉴바 버튼(1개 이상이니까 All로 불러와야함)
+var dropdown = document.querySelectorAll('.mobile_arrow');
 
-dropdown.addEventListener('click', function () {
-  if (dropdownContent.style.display === 'block') {
-    dropdownContent.style.display = 'none';
-    drop_icon1.style.display = 'flex';
-    drop_icon2.style.display = 'none';
-  } else {
-    dropdownContent.style.display = 'block';
-    drop_icon1.style.display = 'none';
-    drop_icon2.style.display = 'flex';
-  }
+//드롭다운 메뉴?컨텐츠내용(1개 이상이니까 All로 불러와야함)
+var dropdownContent = document.querySelectorAll('.mobile_submenu');
+// 아이콘 2개 업과 다운(1개 이상이니까 All로 불러와야함)
+var drop_icon1 = document.querySelectorAll('.down');
+var drop_icon2 = document.querySelectorAll('.up');
+
+// 노드리스트는 객체처럼 생김
+//요소(지정해줘야함), 인덱스 번호 불러옴
+// 총 7개의 클래스가 반복되므로 인덱스 번호가 반복되어야 한다
+dropdown.forEach(function (element, i) {
+  element.addEventListener('click', function () {
+    if (dropdownContent[i].style.display === 'block') {
+      dropdownContent[i].style.display = 'none';
+      drop_icon1[i].style.display = 'inline-flex';
+      drop_icon2[i].style.display = 'none';
+    } else {
+      dropdownContent[i].style.display = 'block';
+      drop_icon1[i].style.display = 'none';
+      drop_icon2[i].style.display = 'inline-flex';
+    }
+  });
 });
 
 //슬라이드
