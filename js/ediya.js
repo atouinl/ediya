5f13ec69c0ef29db4247bdfd9fd8829f1ad5f3cc
atouinl <atouinl@naver.comm>
2025-02-15 14:47:50 +0900

    20250214_769px

--- js/ediya.js
+++ js/ediya.js
@@ -1,2 +1,12 @@
-'use strict';
-const moIcon = document.getElementById('m_icon');
+"use strict";
+function sidemenu() {
+  const moIcon = document.querySelector(".m_icon");
+  const moxIcon = document.querySelector(".m_xicon");
+  if (moIcon.style.display === "none") {
+    moIcon.style.display = "block";
+    moxIcon.style.display = "block";
+  } else {
+    moIcon.style.display = "none";
+    moxIcon.style.display = "none";
+  }
+}
