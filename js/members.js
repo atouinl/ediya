36ab928b2075c9a1bbbb2caced34dd662b8b4a60
atouinl <atouinl@naver.comm>
2025-03-04 19:40:42 +0900

    슬라이드 오류 해결

--- js/members.js
+++ js/members.js
@@ -1,73 +1,80 @@
-'use strict';
-
-// 햄버거 버튼
-const moIcon = document.getElementsByClassName('m_icon');
-// 모바일 전체메뉴
-const mobile_wrap = document.getElementsByClassName('mobile_wrap');
-//x버튼
-const m_xicon = document.getElementsByClassName('m_xicon');
+"use strict";
 
 function sidemenu() {
-  if (moIcon.style.display === 'none') {
-    moIcon.style.display = 'block';
-    m_xicon.style.display = 'block';
+  if (moIcon.style.display === "none") {
+    moIcon.style.display = "block";
+    m_xicon.style.display = "block";
   } else {
-    moIcon.style.display = 'none';
-    m_xicon.style.display = 'none';
+    moIcon.style.display = "none";
+    m_xicon.style.display = "none";
   }
 }
+// 햄버거 버튼
+const moIcon = document.getElementsByClassName("m_icon");
+// 모바일 전체메뉴
+const mobile_wrap = document.getElementsByClassName("mobile_wrap");
+//x버튼
+const m_xicon = document.getElementsByClassName("m_xicon");
 
-moIcon.addEventListener('click', () => {
-  mobile_wrap.style.display = 'flex';
-});
-m_xicon.addEventListener('click', () => {
-  mobile_wrap.style.display = 'none';
-});
+// moIcon.addEventListener("click", () => {
+// mobile_wrap.style.display = "flex";
+// });
+// m_xicon.addEventListener("click", () => {
+// mobile_wrap.style.display = "none";
+// });
+
+moIcon.onclick = function () {
+  mobile_wrap.style.display = "flex";
+};
+
+m_xicon.onclick = function () {
+  mobile_wrap.style.display = "none";
+};
 
 //왼쪽 이미지 드롭다운
 
 //드롭다운 메뉴바 버튼(1개 이상이니까 All로 불러와야함)
-var dropdown = document.querySelectorAll('.mobile_arrow');
+var dropdown = document.querySelectorAll(".mobile_arrow");
 
 //드롭다운 메뉴?컨텐츠내용(1개 이상이니까 All로 불러와야함)
-var dropdownContent = document.querySelectorAll('.mobile_submenu');
+var dropdownContent = document.querySelectorAll(".mobile_submenu");
 // 아이콘 2개 업과 다운(1개 이상이니까 All로 불러와야함)
-var drop_icon1 = document.querySelectorAll('.down');
-var drop_icon2 = document.querySelectorAll('.up');
+var drop_icon1 = document.querySelectorAll(".down");
+var drop_icon2 = document.querySelectorAll(".up");
 
 // 노드리스트는 객체처럼 생김
 //요소(지정해줘야함), 인덱스 번호 불러옴
 // 총 7개의 클래스가 반복되므로 인덱스 번호가 반복되어야 한다
 dropdown.forEach(function (element, i) {
-  element.addEventListener('click', function () {
-    if (dropdownContent[i].style.display === 'block') {
-      dropdownContent[i].style.display = 'none';
-      drop_icon1[i].style.display = 'inline-flex';
-      drop_icon2[i].style.display = 'none';
+  element.addEventListener("click", function () {
+    if (dropdownContent[i].style.display === "block") {
+      dropdownContent[i].style.display = "none";
+      drop_icon1[i].style.display = "inline-flex";
+      drop_icon2[i].style.display = "none";
     } else {
-      dropdownContent[i].style.display = 'block';
-      drop_icon1[i].style.display = 'none';
-      drop_icon2[i].style.display = 'inline-flex';
+      dropdownContent[i].style.display = "block";
+      drop_icon1[i].style.display = "none";
+      drop_icon2[i].style.display = "inline-flex";
     }
   });
 });
 
 //슬라이드
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
+    el: ".swiper-pagination",
+    type: "progressbar",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
