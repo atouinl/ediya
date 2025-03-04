1a5f36174bd6f2d5311f19f07c19eefb9afa26ac
atouinl <atouinl@naver.com>
2025-03-04 18:25:41 +0900

    스와이프 적용중

--- js/members.js
+++ js/members.js
@@ -1,30 +1,28 @@
 'use strict';
+
+// 햄버거 버튼
+const moIcon = document.getElementsByClassName('m_icon');
+// 모바일 전체메뉴
+const mobile_wrap = document.getElementsByClassName('mobile_wrap');
+//x버튼
+const m_xicon = document.getElementsByClassName('m_xicon');
+
 function sidemenu() {
-  const moIcon = document.querySelector('.m_icon');
-  const moxIcon = document.querySelector('.m_xicon');
   if (moIcon.style.display === 'none') {
     moIcon.style.display = 'block';
-    moxIcon.style.display = 'block';
+    m_xicon.style.display = 'block';
   } else {
     moIcon.style.display = 'none';
-    moxIcon.style.display = 'none';
+    m_xicon.style.display = 'none';
   }
 }
 
-// 햄버거 버튼
-const moIcon = document.querySelector('.m_icon');
-// 모바일 전체메뉴
-const mobile_wrap = document.querySelector('.mobile_wrap');
-//x버튼
-const m_xicon = document.querySelector('.m_xicon');
-
-moIcon.onclick = function () {
+moIcon.addEventListener('click', () => {
   mobile_wrap.style.display = 'flex';
-};
-
-m_xicon.onclick = function () {
+});
+m_xicon.addEventListener('click', () => {
   mobile_wrap.style.display = 'none';
-};
+});
 
 //왼쪽 이미지 드롭다운
 
@@ -55,58 +53,25 @@ dropdown.forEach(function (element, i) {
 });
 
 //슬라이드
-const main_swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
   // Optional parameters
-  slidesPerView: 1,
-  spaceBetween: 20,
   direction: 'horizontal',
   loop: true,
-  autoplay: {
-    delay: 2500, // 시간 설정
-    disableOnInteraction: false, // false-스와이프 후 자동 재생
-  },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'progressbar',
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.main_visual_prev',
-    prevEl: '.main_visual_next',
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
   },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-
-const sec1_swiper1 = new Swiper('.sec1_swiper1', {
-  // Optional parameters
-  slidesPerView: 1,
-  spaceBetween: 20,
-  direction: 'horizontal',
-  loop: true,
-  autoplay: {
-    delay: 3000, // 시간 설정
-    disableOnInteraction: false, // false-스와이프 후 자동 재생
-  },
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: '.main_visual_prev',
-  //   prevEl: '.main_visual_next',
+  // scrollbar: {
+  //   el: '.swiper-scrollbar',
   // },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
